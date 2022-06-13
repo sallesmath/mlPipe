d82598087ea3af8d0ce12750b9d0877b6922046f
Matheus Salles <matheusmaciel.salles@gmail.com>
2022-06-13 16:32:09 -0300

    Adding new function: ml.results.graphic - Through this function you´re able to plot and compare different machine learning models

---
+++ R/3-ml.results.graphic.R
@@ -0,0 +1,38 @@
+#' Plotting evaluation metrics from different machine learning models
+#'
+#' @import lattice
+#' @param ml_result
+#' @param graphic
+#' @param ...
+#' @inheritParams lattice::splom
+#' @inheritParams lattice::parallelplot
+#' @inheritParams lattice::bwplot
+#' @inheritParams lattice::dotplot
+#'
+#' @return graphic
+#' @export
+#'
+#' @examples
+#' # upload the necessary packages
+#' library(mlbench)
+#' library(caret)
+#' library(caretEnsemble)
+#' # load data
+#' # table available in caret package database
+#' data(PimaIndiansDiabetes)
+#' # rename dataset to keep code below generic
+#' my_df <- PimaIndiansDiabetes
+#' # now you're able to train your data
+#' my_models <- ml.trainlist(my_df, 0.8, algorithm = c("rf", "lda", "glm", "knn"), metric = "Accuracy", seed = 42, resampling = "cv")
+#' my_models
+#' results <- caret::resamples(my_models)
+#' summary(results)
+#' ml.results.graphic(ml_result = results, graphic = "bwplot")
+
+ml.results.graphic <- function(ml_result, graphic = c("bwplot", "dotplot", "parallelplot", "splom"), ...) {
+  graphic <- match.arg(graphic)
+  if(graphic=="bwplot") return(bwplot(ml_result))
+  if(graphic=="dotplot") return(dotplot(ml_result))
+  if(graphic=="parallelplot") return(parallelplot(ml_result))
+  if(graphic=="splom") return(splom(ml_result))
+}
