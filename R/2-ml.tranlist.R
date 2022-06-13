af5d5176a70f8f6cb8040e0b112d5b8c5dc36988
Matheus Salles <matheusmaciel.salles@gmail.com>
2022-06-13 15:16:21 -0300

    New function: ml.trainlist
    This function returns a list from multiple machine learning trained models by the caret::train function

--- R/2-ml.tranlist.R
+++ R/2-ml.tranlist.R
@@ -1,41 +1,50 @@
-#' Configuration of input file and training data with a machine learning model
+#' Returning a list from multiple machine learning trained models
 #'
-#' @import caret mlbench
+#' @import caret mlbench caretEnsemble
 #' @param dataset a dataframe
 #' @param train_percent numerical value (percentage, in decimals) you want for training data
-#' @param resampling specific method from caret::trainControl
-#' @param algorithm specific method from caret::trainControl
+#' @param algorithm set of methods from caret::trainControl
 #' @param metric specific evaluation metric from caret::trainControl
 #' @param seed specific seed value
+#' @param resampling specific method from caret::trainControl
+#' @param ...
 #' @inheritParams caret::trainControl
 #' @inheritParams caret::train
+#' @inheritParams caretEnsemble::caretList
 #'
-#' @return an object is returned of class train
+#' @return a list of objects of class 'train' (from caret::train)
 #' @export
 #'
 #' @examples
 #' # upload the necessary packages
 #' library(mlbench)
 #' library(caret)
+#' library(caretEnsemble)
 #' # load data
 #' # table available in caret package database
 #' data(PimaIndiansDiabetes)
 #' # rename dataset to keep code below generic
 #' my_df <- PimaIndiansDiabetes
 #' # now you're able to train your data
-#' my_model <- ml.train(my_df, 0.8, "repeatedcv", "lda", "Accuracy", 42)
+#' ml.trainlist(my_df, 0.8, algorithm = c("rf", "lda", "glm", "knn"), metric = "Accuracy", seed = 42, resampling = "cv")
 #' my_model
 
-ml.train <- function(dataset, train_percent, algorithm, metric, seed, resampling) {
+ml.trainlist <- function(dataset, train_percent, algorithm, metric, seed, resampling, ...) {
   set.seed(seed) # setting a specific seed
   rows <- sample(nrow(dataset))
+
   shuffled_dataset <- dataset[rows, ] # here we're shuffling our data, to avoid any bias in the analysis
   split <- round(nrow(shuffled_dataset) * train_percent) # setting a training sample from the original dataframe
   first_train_sample <- shuffled_dataset[1:split, ] # here we're setting our training sample
+
   dependent_variable <- first_train_sample[ , ncol(first_train_sample), drop = FALSE] # here we're setting our dependent variable, assuming it's in the last column of the dataframe
   vec_dependent_variable <- dependent_variable[, 1] # transforming our dependent variable into a vector
   final_train_sample <- first_train_sample[,-ncol(first_train_sample)] # here we're removing the column with the dependent variable from our new dataframe with the training data
+
   control <- caret::trainControl(method = resampling) # setting parameters from trainControl
-  model.fit <- caret::train(final_train_sample, vec_dependent_variable, method=algorithm, metric=metric, trControl=control)
-  return(model.fit)
+
+  # now we're going to create a list of several train models from the caret package, by building a list of train objects suitable for ensembling using the caretEnsemble function
+  list_caret <- caretEnsemble::caretList(final_train_sample, vec_dependent_variable, methodList = algorithm, metric = metric, trControl = control)
+
+  return(list_caret) # return your caretList
 }
